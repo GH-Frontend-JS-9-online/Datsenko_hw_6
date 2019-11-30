c1f9064c9cea6f0d4cbbd15d888d90592656261d
Yevhen Datsenko <gk16121983@gmail.com>
2019-11-30 23:10:11 +0200

    refactor hw6

--- src/js/replace.js
+++ src/js/replace.js
@@ -1,19 +1,10 @@
-export default function(array) {
-    let maxValue = Math.max(...array)
-    let minValue = Math.min(...array)
+const replaceMinMax = (array) => {  
     let tempValue = 0
-    let maxValueIndex = 0
-    let minValueIndex = 0
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === minValue) {
-            minValueIndex = i
-        }
-        else if (array[i] === maxValue) {
-            maxValueIndex = i
-        }        
-    }
+    let maxValueIndex = array.indexOf(Math.max(...array))
+    let minValueIndex = array.indexOf(Math.min(...array))   
     tempValue = array[maxValueIndex]
     array[maxValueIndex] = array[minValueIndex]
     array[minValueIndex] = tempValue
     return array
 }
+export default replaceMinMax
